70d965ccc8f6b2451e9139e9c9f02b733f2720a5
Vrukshali Torawane <65615660+Vrukshali-26@users.noreply.github.com>
2021-11-30 16:11:23 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,17 @@
+# provider "civo" {
+#   token = ${{ secrets.CIVO_TOKEN }}
+# }
+
+terraform {
+  required_providers {
+    civo = {
+      source = "civo/civo"
+      version = "0.10.3"
+    }
+  }
+}
+
 provider "civo" {
   token = ${{ secrets.CIVO_TOKEN }}
+  region = "LON1"
 }
