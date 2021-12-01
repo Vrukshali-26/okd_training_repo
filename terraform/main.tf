c9a5f5fff6f70f5d567bfb9c5f691d5d6c999222
Vrukshali Torawane <65615660+Vrukshali-26@users.noreply.github.com>
2021-12-01 09:30:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    civo = {
-      source = "civo/civo"
-      version = "0.10.3"
-    }
-  }
-}
-
-provider "civo" {
-  token = "mpl6XUOkXb3AasxJnxjLCZxPDLCigQiCAUFcF3HO1MyDVKCArH"
-  region = "LON1"
-}
-
 resource "civo_kubernetes_cluster" "cluster" {
     name = "sample2"
     #region = "LON1"
